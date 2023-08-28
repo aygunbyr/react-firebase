7bd7b4adab54fc11e6df8ef5d671935064b79404
Aygün Bayır <turkoaygun@gmail.com>
2023-08-28 15:18:14 +0300

    feat: app bar for main layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 import { useIsLoggedIn } from '../config/hooks';
 
 const Layout = () => {
   const isLoggedIn = useIsLoggedIn();
+
+  const [anchorEl, setAnchorEl] = useState(null);
 
   if (isLoggedIn === null) {
     return <h1>Loading...</h1>;
@@ -10,9 +23,39 @@ const Layout = () => {
     return <Navigate replace to="/sign-in" />;
   }
 
-  console.log('is logged in = ' + isLoggedIn);
+  return (
+    <>
+      <AppBar position="apsolute">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
 
-  return <Outlet />;
+          <IconButton
+            color="inherit"
+            onClick={(e) => {
+              setAnchorEl(e.currentTarget);
+            }}
+            size="large"
+          >
+            <AccountCircleIcon />
+          </IconButton>
+
+          <Menu
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={() => {
+              setAnchorEl(null);
+            }}
+          >
+            <MenuItem onClick={() => setAnchorEl(null)}>Profile</MenuItem>
+            <MenuItem onClick={() => setAnchorEl(null)}>Sign out</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </>
+  );
 };
 
 export default Layout;
